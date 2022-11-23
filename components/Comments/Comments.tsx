1abe85124387f2f510f52d7fc3eed02e6e5311de
Jac Wynn <jacquinwynn91@gmail.com>
2022-11-23 14:24:25 -0500

    add components folder

---
+++ components/Comments/Comments.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+type Data = {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: {
+    image: {
+      png: string;
+      webp: string;
+    };
+    username: string;
+  };
+  replies: {
+    id: number;
+    content: string;
+    createdAt: string;
+    score: number;
+    replyingTo: string;
+    user: {
+      image: {
+        png: string;
+        webp: string;
+      };
+      username: string;
+    };
+  }[];
+}[];
+
+const Comments = (props) => {
+  return <div>{props.comments && <h1>We got data...</h1>}</div>;
+};
+
+export default Comments;
