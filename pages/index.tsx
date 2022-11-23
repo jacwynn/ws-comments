2a46b7511a72e51d58231c52b915576d4232d4bc
Jac Wynn <jacquinwynn91@gmail.com>
2022-11-23 11:52:41 -0500

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Comments App</title>
+        <meta
+          name="description"
+          content="An Interactive Comments Application"
+        />
+      </Head>
+
+      <h1>HomePage</h1>
+    </div>
+  );
+}
